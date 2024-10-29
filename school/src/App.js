e8a18ceedff48e017b75b6ab70e21cac8ce0c1e5
IRFAN SHEIKH <s4sheikh@gmail.com>
2024-10-29 06:05:58 +0500

    School Project Added

---
+++ school/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Courses from './pages/Courses'
+import Home from './pages/Home'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/courses' element={<Courses/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
